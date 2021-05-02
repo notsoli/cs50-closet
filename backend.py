e679001dd12b725e9a56f38d10b4bcb6ca8109d1
Nate Randa <naterandaofficial@gmail.com>
2021-05-02 10:44:49 -0400

    add upload page

--- backend.py
+++ backend.py
@@ -8,17 +8,19 @@ def initialize_database():
     c.exeute('''CREATE TABLE IF NOT EXISTS entries(
         id INT PRIMARY AUTOINCREMENT,
         name TEXT,
-        photo BLOB,
+        photo_path TEXT,
         )''')
     c.execute('''CREATE TABLE IF NOT EXISTS items(
         id INT PRIMARY
         name TEXT
         )''')
     conn.commit()
+    return True, "OK"
 
-def add_item(name, items, photo):
-    c.execute("INSERT INTO entries(name, photo) VALUES(?,?)", name, photo)
+def add_item(name, items, photo_path):
+    c.execute("INSERT INTO entries(name, photo_path) VALUES(?,?)", name, photo_path)
     response = c.execute("SELECT id FROM entries WHERE name=?", name)
     id = response[0]
     for item in items:
         c.execute("INSERT INTO items VALUES(?,?)", id, item)
+    return True, "OK"
--- main.py
+++ main.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        return render_template('upload.html')
+    else:
+        return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
